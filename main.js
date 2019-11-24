4a79cde11ed35ac6eee013eec8c2bec611c9f489
Henry-Peti <iamgadgetboy@gmail.com>
2019-11-25 08:43:29 +1300

    html setup and testing sign up

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Options 
+// Options
 const CLIENT_ID = '797737878705-c4h4s0k5p50h89104ujj4850471kut6g.apps.googleusercontent.com'
 const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly'
@@ -10,3 +10,57 @@ const content = document.getElementById('content')
 const channelForm = document.getElementById('channel-form')
 const channelInput = document.getElementById('channel-input')
 const videoContainer = document.getElementById('video-container')
+
+const defaultChannel = 'techguyweb'
+
+// Load auth2 library
+function handleClientLoad () {
+  gapi.load('client:auth2', initClient)
+}
+
+// Init API client library and set up sign in listeners
+function initClient () {
+  gapi.client.init({
+    discoveryDocs: DISCOVERY_DOCS,
+    clientId: CLIENT_ID,
+    scope: SCOPES
+  }).then(() => {
+    // Listen for sign in state changes
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus)
+    // Handle initial sign in state
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get())
+    authorizeButton.onClick = handleAuthClick
+    signoutButton.onClick = handleSignoutClick
+  })
+}
+
+// Update UI sign in state changes
+function updateSigninStatus (isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none'
+    signoutButton.style.display = 'block'
+    content.style.display = 'block'
+    videoContainer.style.display = 'block'
+    getChannel(defaultChannel)
+  } else {
+    authorizeButton.style.display = 'block'
+    signoutButton.style.display = 'none'
+    content.style.display = 'none'
+    videoContainer.style.display = 'none'
+  }
+}
+
+// Handle login
+function handleAuthClick () {
+  gapi.auth2.getAuthInstance().signIn()
+}
+
+// Handle logout
+function handleSignoutClick () {
+  gapi.auth2.getAuthInstance().signOut()
+}
+
+// Get channel from api
+function getChannel(channel) {
+    console.log(channel)
+}
